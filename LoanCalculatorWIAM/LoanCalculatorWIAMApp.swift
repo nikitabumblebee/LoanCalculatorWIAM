6cf9c84a494a948c6b3ba56ff39091272dd0c819
NikitaBumblebee <nikitashmelev7@gmail.com>
2025-12-18 16:58:45 +0700

    Initial Commit

---
+++ LoanCalculatorWIAM/LoanCalculatorWIAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoanCalculatorWIAMApp.swift
+//  LoanCalculatorWIAM
+//
+//  Created by Nikita Shmelev on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct LoanCalculatorWIAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
